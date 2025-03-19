7d596d2b56557d3fbc05a70a9133ee028372b47b
cureforboredom <87227173+cureforboredom@users.noreply.github.com>
2025-03-19 07:53:42 -0600

    playable

--- sudoku/static/play.js
+++ sudoku/static/play.js
@@ -6,6 +6,8 @@ current_board = [];
 
 have_set_editable = false;
 
+selected_cell = null;
+
 function init() {
   dom_board = document.getElementById("board");
 
@@ -25,7 +27,8 @@ function init() {
       cell.id = i * 9 + j;
       cell.innerHTML = "<p>0</p>";
       cell.onclick = function () {
-        modifyBoard(this);
+        document.getElementById("picker-container").style.display = "flex";
+        selected_cell = this;
       };
 
       row.append(cell);
@@ -33,6 +36,38 @@ function init() {
 
     dom_board.append(row);
   }
+
+  document.getElementById("picker-closer").onclick = function () {
+    document.getElementById("picker-container").style.display = "none";
+  };
+
+  picker = document.getElementById("picker");
+
+  for (let i = 0; i < 3; i++) {
+    row = document.createElement("div");
+    for (let j = 0; j < 3; j++) {
+      cell = document.createElement("div");
+      cell.id = i * 3 + j + 1;
+      cell.classList.add("choice");
+      cell.innerHTML = "<p>" + (i * 3 + j + 1) + "</p>";
+      cell.onclick = function () {
+        modifyBoard(selected_cell, this.id);
+        document.getElementById("picker-container").style.display = "none";
+      };
+      row.append(cell);
+    }
+    picker.append(row);
+  }
+
+  clear = document.createElement("div");
+  clear.id = "clear";
+  clear.innerHTML = "<p>Clear</p>";
+  clear.onclick = function () {
+    modifyBoard(selected_cell, 0);
+    document.getElementById("picker-container").style.display = "none";
+  };
+
+  picker.append(clear);
 
   window.setInterval(update, 1000);
 }
@@ -63,14 +98,14 @@ const update = async () => {
   }
 };
 
-const modifyBoard = async (cell) => {
+const modifyBoard = async (cell, value) => {
   if (cell.classList.contains("editable")) {
     const r = await fetch("/api/modify_board", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify([parseInt(cell.id, 10), 8]),
+      body: JSON.stringify([parseInt(cell.id, 10), value]),
     });
   }
 };
